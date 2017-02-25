99967378bbc94fb620413bedbcc20758ed77f3b2
TRQ1 <trq1.lee@gmail.com>
2017-02-25 15:15:13 +0900

    Update bootstrap-swarm.sh

--- vagrant/scripts/bootstrap-swarm.sh
+++ vagrant/scripts/bootstrap-swarm.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
+# install latest docker-engine
+sudo apt-get -y install apt-transport-https \
+                       ca-certificates
+
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-${UBUNTU_VERSION} \
+       main"
+
 sudo apt-get update
 
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get -y install docker-engine
 
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee /etc/apt/sources.list.d/docker.list
 
-sudo apt-get update
+# install docker-compose
+if ! checkDockerCompose; then
+  sudo sh -c 'curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/ docker-compose'
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
 
-sudo apt-get install -y docker-engine
-
-sudo usermod -aG docker ubuntu
+# add vagrant user to docker group
+usermod -aG docker vagrant
